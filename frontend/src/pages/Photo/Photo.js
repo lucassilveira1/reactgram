6c70087865ef8e9bb75c8db701936d323b338312
Lucas Silveira <lucas.silveira0516@gmail.com>
2024-03-15 18:31:41 -0300

    sending project

---
+++ frontend/src/pages/Photo/Photo.js
@@ -0,0 +1,20 @@
+import "./Photo.css";
+
+import { uploads } from "../../utils/config";
+
+// components
+import Message from "../../components/Message/Message";
+import { Link } from "react-router-dom";
+
+// Hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+// Redux
+
+const Photo = () => {
+  return <div>Photo</div>;
+};
+
+export default Photo;
---
+++ frontend/src/services/photoService.js
@@ -0,0 +1,82 @@
+import { api, requestConfig } from "../utils/config";
+
+// Publish an user photo
+const publishPhoto = async (data, token) => {
+  const config = requestConfig("POST", data, token, true);
+
+  try {
+    const res = await fetch(api + "/photos", config)
+      .then((res) => res.json())
+      .catch((err) => err);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// get user photos
+const getUserPhotos = async (id, token) => {
+  const config = requestConfig("GET", null, token);
+
+  try {
+    const res = await fetch(api + "/photos/user/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Delete photo
+const deletePhoto = async (id, token) => {
+  const config = requestConfig("DELETE", null, token);
+
+  try {
+    const res = await fetch(api + "/photos/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Update a photo
+export const updatePhoto = async (data, id, token) => {
+  const config = requestConfig("PUT", data, token);
+
+  try {
+    const res = await fetch(api + "/photos/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Get a photo by id
+const getPhoto = async (id) => {
+  const config = requestConfig("GET");
+
+  try {
+    const res = await fetch(api + "/photos/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const photoService = {
+  publishPhoto,
+  getUserPhotos,
+  deletePhoto,
+  updatePhoto,
+  getPhoto,
+};
+
+export default photoService;
